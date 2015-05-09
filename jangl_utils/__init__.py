f1b45f4dfd0e917d9d1f28485b3f48029612d5f0
Ryan Anguiano <ryan.anguiano@gmail.com>
2015-05-08 22:47:02 -0400

    Project init

---
+++ jangl_utils/__init__.py
@@ -0,0 +1,38 @@
+import os
+
+__all__ = ('VERSION', 'get_version')
+
+VERSION = '0.1'
+
+
+def _get_git_revision(path):
+    revision_file = os.path.join(path, 'refs', 'heads', 'master')
+    if not os.path.exists(revision_file):
+        return None
+    fh = open(revision_file, 'r')
+    try:
+        return fh.read().strip()[:7]
+    finally:
+        fh.close()
+
+
+def get_revision():
+    """
+    :returns: Revision number of this branch/checkout, if available. None if
+        no revision number can be determined.
+    """
+    package_dir = os.path.dirname(__file__)
+    checkout_dir = os.path.normpath(os.path.join(package_dir, os.pardir))
+    path = os.path.join(checkout_dir, '.git')
+    if os.path.exists(path):
+        return _get_git_revision(path)
+    return None
+
+
+def get_version():
+    base = VERSION
+    if __build__:
+        base = '%s (%s)' % (base, __build__)
+    return base
+
+__build__ = get_revision()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='jangl-utils',
+    version='0.1',
+    packages=['jangl_utils'],
+    include_package_data=True,
+    url='https://github.com/revpoint/jangl-utils',
+    author='RevPoint Media LLC',
+    author_email='jangl@revpointmedia.com',
+    description="Common utilities shared among Jangl services.",
+)

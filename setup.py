699b166c106dacc48dff62c22ab750b2e24deb09
Ryan Anguiano <ryan.anguiano@gmail.com>
2015-07-22 20:09:59 -0400

    Add bumpversion

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'boto',
+        'bumpversion',
         'django-storages',
         'djangorestframework',
         'prettyconf',

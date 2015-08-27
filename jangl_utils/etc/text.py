6c758a8e58042b81d106857dfa408d3f8f8cfecc
Ryan Anguiano <ryan.anguiano@gmail.com>
2015-08-27 14:23:36 -0400

    Add text formatting

--- jangl_utils/etc/text.py
+++ jangl_utils/etc/text.py
@@ -17,3 +17,24 @@ def slugify(value):
     value = re.sub('[^\w\s-]', '', value).strip().lower()
     return mark_safe(re.sub('[-\s]+', '_', value))
 slugify = allow_lazy(slugify, six.text_type, SafeText)
+
+
+def format_percent(num):
+    if not num:
+        return '-'
+    return '{:0.2f}%'.format(num)
+
+
+def format_dollars(num):
+    if not num:
+        return '-'
+    return '${:0.2f}'.format(num)
+
+
+def format_time(num):
+    if not num:
+        return '-'
+    num = int(num)
+    mins = num / 60
+    secs = num % 60
+    return '{:01d}:{:02d}'.format(mins, secs)

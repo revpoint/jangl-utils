598550142f9359a20126a61c0e5b57f601b0fee4
Ryan Anguiano <ryan.anguiano@gmail.com>
2015-09-21 12:47:28 -0400

    Update kafka settings

--- jangl_utils/kafka/settings.py
+++ jangl_utils/kafka/settings.py
@@ -1,6 +1,7 @@
-from prettyconf import config
+from django.conf import settings
 
-BROKER_URL = config('KAFKA_BROKER_URL', default=None)
-ZOOKEEPER_URL = config('KAFKA_ZOOKEEPER_URL', default=None)
-SCHEMA_REGISTRY_URL = config('KAFKA_SCHEMA_REGISTRY_URL', default=None)
-SCHEMA_MICROSERVICE = config('KAFKA_SCHEMA_MICROSERVICE', default=None)
+
+BROKER_URL = getattr(settings, 'KAFKA_BROKER_URL', default=None)
+ZOOKEEPER_URL = getattr(settings, 'KAFKA_ZOOKEEPER_URL', default=None)
+SCHEMA_REGISTRY_URL = getattr(settings, 'KAFKA_SCHEMA_REGISTRY_URL', default=None)
+SCHEMA_MICROSERVICE = getattr(settings, 'KAFKA_SCHEMA_MICROSERVICE', default=None)

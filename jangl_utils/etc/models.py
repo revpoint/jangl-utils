03bad6d1bab4a7c7b94e4b9e67136b54500c4bf4
Ryan Anguiano <ryan.anguiano@gmail.com>
2016-03-28 02:54:37 -0400

    Add queryset iterator

---
+++ jangl_utils/etc/models.py
@@ -0,0 +1,12 @@
+import gc
+
+
+def queryset_iterator(queryset, chunksize=1000):
+    pk = 0
+    last_pk = queryset.order_by('-pk')[0].pk
+    queryset = queryset.order_by('pk')
+    while pk < last_pk:
+        for row in queryset.filter(pk__gt=pk)[:chunksize]:
+            pk = row.pk
+            yield row
+        gc.collect()

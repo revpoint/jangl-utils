55798e6bb665182d7832562dc0028a7f7d072fcb
Ryan Anguiano <ryan.anguiano@gmail.com>
2016-08-01 05:23:54 -0400

    Fix django dependency

--- jangl_utils/kafka/settings.py
+++ jangl_utils/kafka/settings.py
@@ -1,16 +1,7 @@
-try:
-    from django.conf import settings
-except ImportError:
-    settings = None
-
 from prettyconf import config
 
 
-BROKER_URL = getattr(settings, 'KAFKA_BROKER_URL',
-                     config('KAFKA_BROKER_URL', default=None))
-ZOOKEEPER_URL = getattr(settings, 'KAFKA_ZOOKEEPER_URL',
-                        config('KAFKA_ZOOKEEPER_URL', default=None))
-SCHEMA_REGISTRY_URL = getattr(settings, 'KAFKA_SCHEMA_REGISTRY_URL',
-                              config('KAFKA_SCHEMA_REGISTRY_URL', default=None))
-SCHEMA_MICROSERVICE = getattr(settings, 'KAFKA_SCHEMA_MICROSERVICE',
-                              config('KAFKA_SCHEMA_MICROSERVICE', default=None))
+BROKER_URL = config('KAFKA_BROKER_URL', default=None)
+ZOOKEEPER_URL = config('KAFKA_ZOOKEEPER_URL', default=None)
+SCHEMA_REGISTRY_URL = config('KAFKA_SCHEMA_REGISTRY_URL', default=None)
+SCHEMA_MICROSERVICE = config('KAFKA_SCHEMA_MICROSERVICE', default=None)

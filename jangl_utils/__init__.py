96f5c9c9911016462e56fe72c64497c90c4690c4
Ryan Anguiano <ryan.anguiano@gmail.com>
2016-10-27 20:10:19 -0400

    Bump version: 0.7.16 → 0.7.17

--- jangl_utils/__init__.py
+++ jangl_utils/__init__.py
@@ -2,7 +2,7 @@ import os
 
 __all__ = ('VERSION', 'get_version')
 
-VERSION = '0.7.16'
+VERSION = '0.7.17'
 
 
 def _get_git_revision(path):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jangl-utils',
-    version='0.7.16',
+    version='0.7.17',
     packages=find_packages(),
     package_data={'jangl_utils': ['templates/bootstrap/*']},
     install_requires=[

b5886dc440a06352032e52e3e1ac8bb2b5cc8c8c
Ryan Anguiano <ryan.anguiano@gmail.com>
2016-10-28 18:31:01 -0400

    Bump version: 0.8.5 → 0.8.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jangl-utils',
-    version='0.8.5',
+    version='0.8.6',
     packages=find_packages(),
     package_data={'jangl_utils': ['templates/bootstrap/*']},
     install_requires=[

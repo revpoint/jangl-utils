4584295677c9f7e6f1899998e4a168c5b0b6c4a6
Ryan Anguiano <ryan.anguiano@gmail.com>
2016-12-05 22:26:30 -0500

    Add colorlog settings

--- jangl_utils/log_settings.py
+++ jangl_utils/log_settings.py
@@ -1,4 +1,14 @@
-def generate_colorlog_settings(project_name, verbose=False):
+
+
+def generate_colorlog_settings(project_name, verbose=False, extra_packages=None,
+                               project_level='INFO', extra_level='WARNING', verbose_level='DEBUG'):
+    project_level = verbose_level if verbose else project_level
+    extra_level = verbose_level if verbose else extra_level
+
+    packages = [(project_name, project_level), ('jangl_utils', extra_level)]
+    if extra_packages is not None:
+        packages += [(package, extra_level) for package in extra_packages]
+
     return {
         'version': 1,
         'disable_existing_loggers': False,
@@ -12,17 +22,10 @@ def generate_colorlog_settings(project_name, verbose=False):
             'console': {
                 'class': 'logging.StreamHandler',
                 'formatter': 'color',
-                'level': 'DEBUG' if verbose else 'INFO',
+                'level': project_level,
             }
         },
         'loggers': {
-            project_name: {
-                'handlers': ['console'],
-                'level': 'DEBUG' if verbose else 'INFO',
-            },
-            'jangl_utils': {
-                'handlers': ['console'],
-                'level': 'DEBUG' if verbose else 'WARNING',
-            },
+            name: {'handlers': ['console'], 'level': level} for name, level in packages
         },
     }

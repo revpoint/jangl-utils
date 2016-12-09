cbb0538c5b1e4bea59bbaecb5315b31b14a70310
Ryan Anguiano <ryan.anguiano@gmail.com>
2016-12-08 20:21:25 -0500

    Add profile logging

--- jangl_utils/profile.py
+++ jangl_utils/profile.py
@@ -9,33 +9,30 @@ try:
 except ImportError:
     import StringIO
 
-try:
-    from django.conf import settings
-    if not settings.configured:
-        settings = None
-except ImportError:
-    settings = None
-
 logger = logging.getLogger(__name__)
 
 
-def log_cprofile(func=None, action='stats', sort='cumulative', head=20, sample_rate=1.0, callback=logger.debug):
+def log_cprofile(func=None, action='stats', head=20, sample_rate=None, sort='cumulative',
+                 callback=None, log_level='debug'):
     """Decorator to log cProfile output of a method
 
     :param func: Function to profile
     :param action: The profile data to output (stats, callees, callers)
+    :param head: Number of lines to output. None or 0 shows all lines.
     :param sort: One or more ``pstats.Stats`` sort columns (cumulative, ncalls, time, stdname, filename, ...)
-    :param head: Number of lines to output
     :param sample_rate: Number between 0 and 1 that determines how often the function is profiled.
-    :param callback: The callback to log the final output
-    :return: If settings.ENABLE_PROFILING is or django is not installed, return a profiled function.
+    :param callback: The callback to log the final output. Default is the function's module logger
+    :param log_level: The log level of the default callback
+    :return: If settings.ENABLE_PROFILING or Django is not installed, return a profiled function.
      Otherwise, return original function
     """
+    sort_args = sort if isinstance(sort, (list, tuple)) else (sort,)
+
     def decorator(_func):
         @functools.wraps(_func)
         def profiled_func(*args, **kwargs):
             # If random number between 0 and 1 > sample rate, skip profiling
-            if random.random() > sample_rate:
+            if should_not_sample(sample_rate):
                 return _func(*args, **kwargs)
 
             profile = cProfile.Profile()
@@ -46,23 +43,27 @@ def log_cprofile(func=None, action='stats', sort='cumulative', head=20, sample_r
                 profile.disable()
                 return result
             finally:
-                output = StringIO.StringIO()
-                sort_args = sort if isinstance(sort, (list, tuple)) else (sort,)
-
                 # Sort stats and print to output
-                stats = pstats.Stats(profile, stream=output).sort_stats(*sort_args)
-                getattr(stats, 'print_' + action)()
-                log_stats = output.getvalue()
-                output.close()
+                with StringIO.StringIO() as out_buffer:
+                    stats = pstats.Stats(profile, stream=out_buffer).sort_stats(*sort_args)
+                    getattr(stats, 'print_' + action)()
+                    output = out_buffer.getvalue()
 
-                # If head, get top lines
+                # If head, get top lines + 5 header lines
                 if head and head > 0:
-                    log_stats = '\n'.join(log_stats.splitlines()[:head])
+                    output = '\n'.join(output.splitlines()[:head + 5])
 
-                logger.info('Logging cProfile for: ' + _func.__name__)
-                callback('\n' + log_stats + '\n\n')
+                logger.info('Logging cProfile for: {}({})[{}]'.format(
+                    _func.__name__,
+                    _func.func_code.co_filename,
+                    _func.func_code.co_firstlineno
+                ))
 
-        if settings is None or settings.ENABLE_PROFILING:
+                # Send log output to handler
+                log_stats = get_log_handler(_func, callback, log_level)
+                log_stats('\n' + output + '\n\n')
+
+        if profiling_is_enabled():
             return profiled_func
         else:
             return _func
@@ -70,3 +71,35 @@ def log_cprofile(func=None, action='stats', sort='cumulative', head=20, sample_r
     if func is None:
         return decorator
     return decorator(func)
+
+
+try:
+    from django.conf import settings as django_settings
+    if not django_settings.configured:
+        django_settings = None
+except ImportError:
+    django_settings = None
+
+
+def profiling_is_enabled():
+    if django_settings:
+        return getattr(django_settings, 'ENABLE_PROFILING', False)
+    return True
+
+
+def should_not_sample(sample_rate):
+    if sample_rate is None:
+        if django_settings:
+            sample_rate = getattr(django_settings, 'PROFILING_DEFAULT_SAMPLE_RATE', 1.0)
+        else:
+            sample_rate = 1.0
+
+    return random.random() > sample_rate
+
+
+def get_log_handler(_func, callback, log_level):
+    if callback is None:
+        _logger = logging.getLogger(_func.__module__)
+        return getattr(_logger, log_level)
+    else:
+        return callback

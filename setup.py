2527375941357d0a0ee444ea1895804d881a9eec
Ryan Anguiano <ryan.anguiano@gmail.com>
2017-03-10 18:16:23 -0500

    Bump version: 0.9.22 → 0.9.23

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jangl-utils',
-    version='0.9.22',
+    version='0.9.23',
     packages=find_packages(),
     package_data={'jangl_utils': ['templates/bootstrap/*']},
     install_requires=[

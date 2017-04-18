d9dab9ab92436fada20ea30a61672354ec08b6df
Ryan Anguiano <ryan.anguiano@gmail.com>
2017-04-18 14:35:58 -0400

    Fix kafka settings

--- jangl_utils/kafka/utils.py
+++ jangl_utils/kafka/utils.py
@@ -8,8 +8,6 @@ def generate_client_settings(initial_settings, user_settings):
     for key, val in user_settings.iteritems():
         if val is None:
             continue
-        if '.' not in key:
-            continue
         logger.debug('using kafka setting {}: {}'.format(key, val))
         if key.startswith('topic.'):
             settings['default.topic.config'][key[6:]] = val

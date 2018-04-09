b2c28d9bb827a042946a4b8e6491a96d079f6c9c
Ryan Anguiano <ryan.anguiano@gmail.com>
2018-04-09 18:07:46 -0400

    Fix import error

--- jangl_utils/backend_api/old.py
+++ jangl_utils/backend_api/old.py
@@ -11,7 +11,7 @@ from django.utils import six
 from json import dumps as to_json
 import requests
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3 import Retry
+from urllib3 import Retry
 from jangl_utils import logger, settings, VERSION
 from jangl_utils.etc.json import _datetime_decoder
 

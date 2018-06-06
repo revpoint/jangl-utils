45c8488740c56c648ef1f320ffac5dc58a09acd5
Ryan Anguiano <ryan.anguiano@gmail.com>
2018-06-06 13:33:50 -0400

    Bump version: 0.13.6 → 0.13.7

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ DEV_REQUIREMENTS = [
 
 setup(
     name='jangl-utils',
-    version='0.13.6',
+    version='0.13.7',
     packages=find_packages(),
     package_data={'jangl_utils': ['templates/bootstrap/*']},
     install_requires=INSTALL_REQUIREMENTS,

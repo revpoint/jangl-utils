a42719eada653fc543da8837fde1b8337f5b2579
Ryan Anguiano <ryan.anguiano@gmail.com>
2018-09-10 19:24:02 -0400

    Bump version: 0.13.10 → 0.13.11

--- jangl_utils/__init__.py
+++ jangl_utils/__init__.py
@@ -3,7 +3,7 @@ import platform
 
 __all__ = ('VERSION', 'get_version')
 
-VERSION = '0.13.10'
+VERSION = '0.13.11'
 IS_PYPY = platform.python_implementation() == 'PyPy'
 
 
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ DEV_REQUIREMENTS = [
 
 setup(
     name='jangl-utils',
-    version='0.13.10',
+    version='0.13.11',
     packages=find_packages(),
     package_data={'jangl_utils': ['templates/bootstrap/*']},
     install_requires=INSTALL_REQUIREMENTS,

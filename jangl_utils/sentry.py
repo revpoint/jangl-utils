2fbb9d77bfb43e4bf12be184f3a99c412c451ee2
Ryan Anguiano <ryan.anguiano@gmail.com>
2019-11-18 17:52:24 -0500

    Update sentry package

--- jangl_utils/sentry.py
+++ jangl_utils/sentry.py
@@ -1,19 +1,15 @@
 from contextlib import contextmanager
 
-from django.conf import settings
-
-sentry_client = None
+sentry_sdk = None
 try:
-    from raven.contrib.django.models import get_client
-    if getattr(settings, 'SENTRY_URL'):
-        sentry_client = get_client()
+    import sentry_sdk
 except ImportError:
     pass
 
 
 def captureException(*args, **kwargs):
-    if sentry_client:
-        return sentry_client.captureException(*args, **kwargs)
+    if sentry_sdk:
+        return sentry_sdk.capture_exception(*args, **kwargs)
 
 
 @contextmanager
